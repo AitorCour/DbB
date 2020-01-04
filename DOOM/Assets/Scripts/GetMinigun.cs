445f8224020f9d6eb70a73b954e51745abdc8448
DESKTOP-DQHGR6Q\aitor <aitorcour@gmail.com>
2020-01-04 02:56:19 +0100

    puesto armas que se recojen con su respectivo modelo. empezar a hacer los boost del player. ponerlos en el player behaviour

--- DOOM/Assets/Scripts/GetMinigun.cs
+++ DOOM/Assets/Scripts/GetMinigun.cs
@@ -6,14 +6,32 @@ public class GetMinigun : MonoBehaviour
 {
     private Gun gun;
     public int weapon;
+    public int maxWeapons;
+    public GameObject[] mesh;
+    private MeshRenderer child;
     // Start is called before the first frame update
     void Start()
     {
         gun = GameObject.FindGameObjectWithTag("Player").GetComponent<Gun>();
+        child = GetComponentInChildren<MeshRenderer>();
+        child.enabled = false;
         weapon = Random.Range(0, 4);
         Debug.Log(weapon);
+        SetWeapon();
     }
-
+    private void SetWeapon()
+    {
+        Quaternion rot = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+        for (int i = 0; i < maxWeapons; i++)
+        {
+            if(i == weapon)
+            {
+                GameObject b = Instantiate(mesh[i], transform.position, /*Quaternion.identity*/ rot, transform);
+                b.name = "gun_" + i;
+            }
+        }
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
